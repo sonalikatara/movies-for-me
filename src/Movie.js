152d39066d5e7a45c6a41030db2dce97f27299a4
Sonali Shukla <sonalikatara@gmail.com>
2020-09-16 01:30:59 -0700

    feat: display Movie list

---
+++ src/Movie.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class Movie extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <div className="Movie">
+        <div className="Movie-img">
+          <img
+            src={"https://image.tmdb.org/t/p/w1280/" + movie.backdrop_path}
+            alt={movie.title}
+          />
+        </div>
+        <div className="Movie-details">
+          <div ><span className="Movie-title">{movie.title} </span>  {movie.release_date.slice(0, 4)}</div>
+          {/*<div>{movie.genre_ids.join(",")}</div>*/}
+          <div>
+           <b> Votes</b> {movie.vote_average}
+          </div>
+          <div> {`${movie.overview.slice(0, 80)}...`}</div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Movie;
